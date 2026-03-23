fix: correct follower position calculation based on leader yaw

src/follower_drone/follower_drone/formation.py
@@ -21,10 +21,10 @@ class Formation:
         distance = self.distance * layer
         target_radian = yaw_leader + np.pi + theta
 
-        target_x = (x_leader + distance * np.cos(target_radian)) - self.node.monitoring_msg.rtk_n
-        target_y = (y_leader + distance * np.sin(target_radian)) - self.node.monitoring_msg.rtk_e
+        target_x = (x_leader + distance * np.cos(target_radian)) - self.node.ref_ned[0]
+        target_y = (y_leader + distance * np.sin(target_radian)) - self.node.ref_ned[1]
 
-        return [target_x, target_y, self.node.leader_pose_msg.pos_z]
+        return [target_x, target_y, min(self.node.leader_pose_msg.pos_z, -0.1)]
 
     def calculate_yaw(self):
         if self.drone_id % 2 == 0: